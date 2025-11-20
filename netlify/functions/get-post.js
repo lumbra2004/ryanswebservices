72d94dac6d3467810e6c09edd1a1696ebf5df8ef
lumbra2004 <143296336+lumbra2004@users.noreply.github.com>
2025-11-20 23:39:17 +0100

    f

--- netlify/functions/get-post.js
+++ netlify/functions/get-post.js
@@ -4,9 +4,8 @@ export default async (event, context) => {
   const sql = neon();
   const postId = event.queryStringParameters?.id || 1;
   const [post] = await sql`SELECT * FROM posts WHERE id = ${postId}`;
-  return {
-    statusCode: 200,
-    body: JSON.stringify(post),
+  return new Response(JSON.stringify(post), {
+    status: 200,
     headers: { 'Content-Type': 'application/json' }
-  };
+  });
 };
